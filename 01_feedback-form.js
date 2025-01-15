b9a61385ee98ab805c9e36505234e524b86a4a48
Vyborova <74511438+Vyborova@users.noreply.github.com>
2025-01-15 12:04:17 +0300

    Update 01_feedback-form.js

--- 01_feedback-form.js
+++ 01_feedback-form.js
@@ -4,19 +4,19 @@ describe("Форма обратной связи", () => {
     });
 
     it("Успешная отправка формы с корректными данными", () => {
-        // Заполнение полей формы
+       
         cy.get("input.control___e16bbac759474cb49f55").first().type("Мартин");
-        cy.get("input.control___e16bbac759474cb49f55").eq(1).type("ivan@example.com"); // Email второй элемент
-        cy.get(".title___bb675eac964120e07a92").click(); // Открываем выпадающий список
-        cy.contains("Заказ").click(); // Выбираем нужный элемент из выпадающего списка
+        cy.get("input.control___e16bbac759474cb49f55").eq(1).type("ivan@example.com"); 
+        cy.get(".title___bb675eac964120e07a92").click(); 
+        cy.contains("Заказ").click(); 
         cy.get("textarea.control___e16bbac759474cb49f55").type("Это тестовое сообщение.");
 
-        // Нажатие на кнопку отправки
-        cy.get("span.buttonText.contents___fcd7c5db228570626cd4").click(); // Замените селектор на ваш
+      
+        cy.get("span.buttonText.contents___fcd7c5db228570626cd4").click(); 
 
-        // Проверка отображения сообщения об успешной отправке
-        cy.get(".content___dc5e560ce80ecb1e7d40") // Замените селектор на ваш
-            .should("be.visible") // Проверка, что сообщение видно
-            .and("contain", "Благодарим за обращение!"); // Проверка текста сообщения
+    
+        cy.get(".content___dc5e560ce80ecb1e7d40") 
+            .should("be.visible") 
+            .and("contain", "Благодарим за обращение!"); 
     });
 });
